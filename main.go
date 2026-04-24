1063a679537255e1eed4d5386b329273c6f0d2ad
Sreeja Panyala <sreejapanyala24@gmail.com>
2026-04-24 18:34:26 -0500

    add city to greet func

--- main.go
+++ main.go
@@ -2,19 +2,24 @@ package main
 
 import "fmt"
 
-func greet(name string, age int) {
+func greet(name string, age int, city string) {
 	fmt.Println("Hello,", name)
 	fmt.Println("You are", age, "years old")
+	fmt.Println("You are from ", city)
 }
 
 func main() {
 	var name string
 	var age int
+	var city string
 	fmt.Println("Enter your name?")
 	fmt.Scanln(&name)
 
 	fmt.Println("Enter your age?")
 	fmt.Scanln(&age)
 
-	greet(name, age)
+	fmt.Println("Enter your city?")
+	fmt.Scanln(&city)
+
+	greet(name, age, city)
 }
