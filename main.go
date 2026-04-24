9a582dd550f6a42047313f5aba6da573dce9ab7b
sreejapanyala24 <sreejapanyala24@gmail.com>
2026-04-24 18:41:14 -0500

    Feature/development (#3) - new enhancements
    * add func to print greet
    
    * add bug intentionally - remove name from greet func
    
    * bug fix - add name to greet func
    
    * add age to greet func
    
    * add city to greet func

--- main.go
+++ main.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
+func greet(name string, age int, city string) {
+	fmt.Println("Hello,", name)
+	fmt.Println("You are", age, "years old")
+	fmt.Println("You are from ", city)
+}
+
 func main() {
-	fmt.Println("Hello World")
+	var name string
+	var age int
+	var city string
+	fmt.Println("Enter your name?")
+	fmt.Scanln(&name)
+
+	fmt.Println("Enter your age?")
+	fmt.Scanln(&age)
+
+	fmt.Println("Enter your city?")
+	fmt.Scanln(&city)
+
+	greet(name, age, city)
 }
